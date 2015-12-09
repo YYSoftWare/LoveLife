fcaa5c0dbc2e2eaacf1cb4be94dfdc550bdfbca4
YYSoftWare <yangyang@yangyang.local>
2015-12-09 16:19:05 +0800

    Initial Commit

---
+++ LoveLife/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoveLife
+//
+//  Created by 杨阳 on 15/12/9.
+//  Copyright (c) 2015年 yangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoveLife/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoveLife
+//
+//  Created by 杨阳 on 15/12/9.
+//  Copyright (c) 2015年 yangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
